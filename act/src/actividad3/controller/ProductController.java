d8931d1a0555a72214aea38b44fb16aae57d03bc
MAHV852 <mahv852@gmail.com>
2025-02-01 21:56:57 +0000

    Actividad 3 completada

---
+++ act/src/actividad3/controller/ProductController.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductController {
+    private List<Product> products = new ArrayList<>();
+
+
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println("Producto agregado: " + product.getName());
+    }
+
+
+    public void showProducts() {
+        if (products.isEmpty()) {
+            System.out.println("No hay productos en el inventario.");
+            return;
+        }
+        System.out.println("\n=== Catálogo de Productos ===");
+        for (Product product : products) {
+            product.showDetails();
+            System.out.println("--------------------");
+        }
+    }
+
+    public Product findProductByName(String name) {
+        for (Product product : products) {
+            if (product.getName().equalsIgnoreCase(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
